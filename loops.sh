e31bbca4516f3983db8c8e2fe173bf56f2695565
S-kanhere <shubh.kanhere@gmail.com>
2024-10-05 20:47:29 +0530

    added conditions

--- loops.sh
+++ loops.sh
@@ -1,19 +1,46 @@
 #!/bin/bash
 
+
+DATE=$(date +%F)
+LOGSDIR=/mnt/c/Users/shubh/Documents/DevOps/shellscripting/shellscript-logs
+
+#/mnt/c/Users/shubh/Documents/DevOps/shellscripting/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
+
 R="\e[31m"
 N="\e[0m"
+Y="\e[33m"
+G="\e[32m"
 
-if [ USERID -ne 0 ];
+if [ $USERID -ne 0 ];
 then
     echo -e "$R ERROR:: PLEASE RUN THIS COMMAND WITH ROOT ACCESS $N" 
     exit 1
 fi
 
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then 
+        echo -e "Installing $2 ... $R Failure $N"
+    else
+        echo -e "Installing $2 ... $G Success $N"
+}
+
 #all args are in $@
 
 for i in $@
 do
-    apt install $i -y 
+    apt list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, let's install it"
+        apt install $i -y &>>$LOGFILE
+    else
+        echo -e "$Y $i is already installed $N" 
+    fi
+
+
 done    
 
