74b2f19f6d7d85e23710242019db60dd33c17aba
S-kanhere <shubh.kanhere@gmail.com>
2024-10-05 21:53:25 +0530

    added conditions

---
+++ delete-old-logs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+APP_LOGS_DIR=/mnt/c/Users/shubh/Documents/DevOps/shellscripting/app-logs
+DATE=$(date +%F)
+LOGSDIR=/mnt/c/Users/shubh/Documents/DevOps/shellscripting/shellscript-logs
+
+#/mnt/c/Users/shubh/Documents/DevOps/shellscripting/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+
+echo $FILES_TO_DELETE
