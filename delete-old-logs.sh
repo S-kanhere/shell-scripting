e07d3f0b9ecb31a2960631c7dcbaad17f2ba0d87
S-kanhere <shubh.kanhere@gmail.com>
2024-10-11 14:22:15 +0530

    scripts added

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -6,7 +6,7 @@ LOGSDIR=/mnt/c/Users/shubh/Documents/DevOps/shellscripting/shellscript-logs
 
 #/mnt/c/Users/shubh/Documents/DevOps/shellscripting/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$0-$DATE.log
+LOGFILE=$LOGSDIR/${SCRIPT_NAME%.*}-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
@@ -14,6 +14,6 @@ echo "$FILES_TO_DELETE"
 
 while read line
 do
-    echo "Deleting $line" &>> $LOGFILE
-    rm -rf $line
-done <<< $FILES_TO_DELETE
+    echo "Deleting $line" >> "$LOGFILE" 2>&1
+    rm -rf "$line"
+done <<< "$LOGFILE"
